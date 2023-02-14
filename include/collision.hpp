869cbfb37fec2ab0952292478abe3d94d5755a1b
Pedro Colle <pedro.h.b.colle@gmail.com>
2023-02-13 22:22:48 -0300

    spacial hash done?

--- include/collision.hpp
+++ include/collision.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <tuple>
+#include <utility>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 #include <memory>
 
 #include "entities.hpp"
@@ -17,30 +18,27 @@ namespace utils{
 		seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 	}
 	//realmente
-	struct tuple_hash : std::unary_function<std::tuple<int,int,int>, std::size_t>{
-		std::size_t operator()(std::tuple<int,int,int> const& e) const {
+	struct pair_hash : std::unary_function<std::pair<int,int>, std::size_t>{
+		std::size_t operator()(std::pair<int,int> const& e) const {
 			std::size_t seed = 0;
-			hash_combine(seed,std::get<0>(e));
-			hash_combine(seed,std::get<1>(e));
-			hash_combine(seed,std::get<2>(e));
+			hash_combine(seed,e.first);
+			hash_combine(seed,e.second);
 			return seed;
 		}
 	};
 	//podia ser facil, mas n~]aoooooooooooooo
-	struct tuple_equal_to : std::binary_function<std::tuple<int,int,int>, std::tuple<int,int,int>, bool>
+	struct pair_equal_to : std::binary_function<std::pair<int,int>, std::pair<int,int>, bool>
 	{
-		bool operator()(std::tuple<int,int,int> const& x, std::tuple<int,int,int> const& y) const
+		bool operator()(std::pair<int,int> const& x, std::pair<int,int> const& y) const
 		{
-			return (std::get<0>(x)==std::get<0>(y) &&
-					std::get<1>(x)==std::get<1>(y) &&
-					std::get<2>(x)==std::get<2>(y));
+			return (x.first == y.first && x.second == y.second);
 		}
 	};
-
+	//for x y cords
 	class SpacialHash {
 		public:
 			//constructor
-			SpacialHash(float cell_x, float cell_y, float cell_z);
+			SpacialHash(float cell_x, float cell_y);
 
 			//insert the shared_ptrs at every cell the entity ocupies,
 			//as cords(x, y) + it's bounding box
@@ -53,16 +51,20 @@ namespace utils{
 			auto remove(std::shared_ptr<entity::Entity> elem) -> int;
 
 			//return a set of all the entities nearby
-			auto get_neighbors(std::shared_ptr<entity::Entity> elem) -> std::unordered_set<std::shared_ptr<entity::Entity>>;
+			auto get_neighbors(std::shared_ptr<entity::Entity> elem)
+				-> std::vector<std::unordered_set<std::shared_ptr<entity::Entity>>&>;
 
 		private:
 			//have it at minimum the size of the biggest enemy
-			float cell_x, cell_y, cell_z;
+			float cell_x, cell_y;
 
 			std::unordered_map<
-				std::tuple<int,int,int>,
+				std::pair<int,int>,
 				std::unordered_set<std::shared_ptr<entity::Entity>>,
-				tuple_hash, tuple_equal_to> map;
+				pair_hash, pair_equal_to> map;
+
+
+			auto get_key(std::shared_ptr<entity::Entity> elem) -> std::pair<int,int>;
 	};
 	
 }
--- src/collision.cpp
+++ src/collision.cpp
@@ -3,38 +3,68 @@
 #include <cmath>
 
 namespace utils{
-	SpacialHash::SpacialHash(float cell_x, float cell_y, float cell_z):
-		cell_x(cell_x), cell_y(cell_y), cell_z(cell_z){}
+	SpacialHash::SpacialHash(float cell_x, float cell_y):
+		cell_x(cell_x), cell_y(cell_y){}
+
+
+	auto SpacialHash::get_key(std::shared_ptr<entity::Entity> elem) -> std::pair<int,int>{
+		int center_x = static_cast<int>(floorf(elem->get_x_cords()/cell_x));
+		int center_y = static_cast<int>(floorf(elem->get_y_cords()/cell_y));
+
+		return std::make_pair(center_x,center_y);
+	}
 
 	//insert the shared_ptrs at every cell the entity ocupies,
 	//as cords(x, y) + it's bounding box
 	//return the num of cells it ocupies
 	auto SpacialHash::insert(std::shared_ptr<entity::Entity> elem) -> int {
-		int center_x = static_cast<int>(floorf(elem->get_x_cords()/cell_x));
-		int center_y = static_cast<int>(floorf(elem->get_y_cords()/cell_y));
-		int center_z = static_cast<int>(floorf(elem->get_z_cords()/cell_z));
-
-		auto key = std::make_tuple(center_x,center_y,center_x);
-
-		//does not cointain the key, therefore, has to initialize the unordered_map
+		auto key = get_key(elem);
+		//does not cointain the key, therefore, has to initialize the unordered_set
 		if(!map.count(key)){
-			map[key] = std::unordered_set<std::shared_ptr<entity::Entity>>();
+			std::unordered_set<std::shared_ptr<entity::Entity>> init;
+			map[key] = std::move(init);
 		}
 		map[key].insert(elem);
-
-		//loop over the bounds
-
+		//TODO: Implement large objects that may be in in more than square
+		return 1;
 	}
 	
 	//remove the shared_ptr at the center,
 	//then at every other cell it also ocupies (x, y) + bbox
 	//return the num of cells it removed from
 	auto SpacialHash::remove(std::shared_ptr<entity::Entity> elem) -> int {
-
+		auto key = get_key(elem);
+		//does not cointain the key, therefore the element is not on the map
+		if(!map.count(key)){
+			return 0;
+		}
+		map[key].erase(elem);
+		//TODO: Implement large objects that may be in in more than square
+		return 1;
 	}
 
 	//return a set of all the entities nearby
-	auto SpacialHash::get_neighbors(std::shared_ptr<entity::Entity> elem) -> std::unordered_set<std::shared_ptr<entity::Entity>>{
+	auto SpacialHash::get_neighbors(std::shared_ptr<entity::Entity> elem)
+		-> std::vector<std::unordered_set<std::shared_ptr<entity::Entity>>&>
+	{
+		auto key = get_key(elem);
+		std::vector<std::unordered_set<std::shared_ptr<entity::Entity>>&> neighbors;
+
+		if(map[key].size() > 1){
+			neighbors.push_back(map[key]);
+		}
+		for(int i = -1; i < 2; i++){
+			for(int j = -1; j < 2; j++){
+				if(i == 0 && j == 0){
+					continue;
+				}
+				auto neighbor_key = std::make_pair(key.first + i, key.second + j);
+				if(map.count(neighbor_key))
+					neighbors.push_back(map[neighbor_key]);
+			}
+		}
+
 
 	}
+
 }
